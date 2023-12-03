83c63f8bef689643cdb30c23b3b075c1086358ea
maths9 <devilcaos110@gmail.com>
2023-12-03 20:28:33 -0300

    update

---
+++ ReflexoUsuario.java
@@ -0,0 +1,10 @@
+
+public class ReflexoUsuario {
+    public static void main(String[] args) {
+        Thread.sleep(6);
+
+        int numeroSecreto = (int) (Math.random() * 50);
+
+        System.out.println("AGORA!");
+    }
+}
